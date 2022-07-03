b515fa8811f16c2c7cff0dc563917887cdc59456
bocahkendil <51228319+bocahkendil@users.noreply.github.com>
2022-07-03 09:44:51 +0700

    update method

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Aplikasi Deteksi Gempa Terkini
 import gempaterkini
 
 if __name__ == '__main__':
-    print('Aplikasi Utama')
+    print('Dashboard Monitoring')
     result = gempaterkini.ekstrasi_data()
     gempaterkini.tampilan_data(result)
 
